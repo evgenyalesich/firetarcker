05bd9ea840bfb0819464bf4158e1240b779172c3
evgeny <quasimodo20042001@gmail.com>
2026-01-10 22:05:46 +0300

    Force Cython rebuild to pick latest sources

--- setup.py
+++ setup.py
@@ -7,16 +7,20 @@ import os
 # print(py_files)
 
 setup(
-    ext_modules=cythonize(["FireStorm/modules/FireStorm.py", 
-                           "FireStorm/modules/update_installer.py",
-                           "FireStorm/modules/uploader.py",
-                           "FireStorm/modules/app_gui.py",
-                           "FireStorm/modules/http_client.py",
-                           "FireStorm/modules/log_in_form.py",
-                           "FireStorm/modules/main_window.py",
-                           "FireStorm/modules/polygons.py",
-                           "FireStorm/modules/update_progressbar.py",
-                           "FireStorm/modules/views.py",
-                           "FireStorm/modules/paths_checker.py"
-                           ])
+    ext_modules=cythonize(
+        [
+            "FireStorm/modules/FireStorm.py",
+            "FireStorm/modules/update_installer.py",
+            "FireStorm/modules/uploader.py",
+            "FireStorm/modules/app_gui.py",
+            "FireStorm/modules/http_client.py",
+            "FireStorm/modules/log_in_form.py",
+            "FireStorm/modules/main_window.py",
+            "FireStorm/modules/polygons.py",
+            "FireStorm/modules/update_progressbar.py",
+            "FireStorm/modules/views.py",
+            "FireStorm/modules/paths_checker.py",
+        ],
+        force=True,
+    )
 )
